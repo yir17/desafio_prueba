7904e8eaf3e8e7cf11ea47c28c1e2ccb5960ef2e
Yirlania Torres <yirlatorres_90@hotmail.com>
2021-02-02 13:35:34 -0600

    Proyect

---
+++ assets/css/index.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('a').click(function(event) {
+        console.log(this, this.hash)
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash
+            })
+        }
+    })
+})
